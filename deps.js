7e38ccc9ad4da233b536944f7a747ecd9f192a2a
linux_china <libing.chen@gmail.com>
2021-01-29 16:52:57 -0800

    bug fix for lodash import

--- deps.js
+++ deps.js
@@ -1,4 +1,4 @@
 import {v4} from "https://deno.land/std@0.84.0/uuid/mod.ts";
 import * as base64 from "https://deno.land/std@0.84.0/encoding/base64.ts";
 
-export lodash from "https://cdn.skypack.dev/lodash";
+import lodash from "https://cdn.skypack.dev/lodash";
